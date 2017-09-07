a47fbd755b75a58a12a02c8cfb4c9c8653df75fe
jcmdln <jcmdln@gmail.com>
2017-09-06 21:47:15 -0400

    Basic scaffolding

--- discord-api/info.rkt
+++ discord-api/info.rkt
@@ -1,13 +1,19 @@
-#lang setup/infotab
+#lang info
 
 (define name "discord-api")
+(define version "0.0.0")
+(define homepage "https://github.com/jcmdln/racket-discord-api")
+(define primary-file "main.rkt")
 (define blurb
   (list
    `(p "Racket bindings for the "
        (a ((href "https://discordapp.com")) "Discord")
        "API")))
-(define homepage "https://github.com/jcmdln/racket-discord-api")
-(define primary-file "main.rk")
-(define categories '(api http))
 
-(define deps '("base" "rfc6455"))
+(define categories
+  '(api
+    http))
+
+(define deps
+  '("base"
+    "rfc6455"))
---
+++ discord-api/main.rkt
@@ -0,0 +1,25 @@
+#lang racket/base
+
+(require
+  "endpoints.rkt"
+  "event.rkt"
+  "logging.rkt"
+  "message.rkt"
+  "oauth2.rkt"
+  "ratelimit.rkt"
+  "restapi.rkt"
+  "user.rkt"
+  "voice.rkt"
+  "wsapi.rkt")
+
+(provide
+ (all-from-out "endpoints.rkt")
+ (all-from-out "event.rkt")
+ (all-from-out "logging.rkt")
+ (all-from-out "message.rkt")
+ (all-from-out "oauth2.rkt")
+ (all-from-out "ratelimit.rkt")
+ (all-from-out "restapi.rkt")
+ (all-from-out "user.rkt")
+ (all-from-out "voice.rkt")
+ (all-from-out "wsapi.rkt"))
